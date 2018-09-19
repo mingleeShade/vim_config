c85db17becde2220f1f07c0e990c66385b6b3d02
haiming.li <haiming.li@mokun.net>
2018-09-19 14:19:15 +0800

    修改安装脚本

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+Init=$1
+if [[ $Init="init" ]];then
+    git clone https://github.com/gmarik/Vundle.vim.git vim/bundle/Vundle.vim/
+fi
+
 cp -f vimrc ~/.vimrc
 cp -rf vim ~/.vim
 cp -f astylerc ~/.astylerc
