81a3a7c1ad0615d161318574c965409d8197b3f7
minglee <361977226@qq.com>
2022-01-29 20:37:20 +0800

    修改安装脚本

--- install.sh
+++ install.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-#检查是系统是debian还是centos
-Debian=`lsb_release -d | grep "Debian" | wc -l`
-echo IsDebian=${Debian}
 
-Param=$1
-
+# TODO: 目前先以最简单方式进行安装，后续再完善安装脚本  <29-01-22, lihaiming>
 Usage()
 {
     echo "./run.sh install  # Copy self config to ~/.config/nvim/."
@@ -14,34 +10,5 @@ Usage()
     echo "./run.sh sync     # Copy ~/.config/nvim/ to self config"
 }
 
-init()
-{
-    s
-}
-
-if [[ $Param == "init" ]];then
-    #git clone
-    git clone https://github.com/gmarik/Vundle.vim.git vim/bundle/Vundle.vim/
-    if [ ${Debian} -eq 1 ]; then
-        sudo apt-get install ctags
-        sudo apt-get install cscope
-    else
-        sudo yum install ctags
-        sudo yum install cscope
-    fi
-fi
-
-if [[ $Param == "clear" ]];then
-    rm ~/.vim/ -rf
-    rm ~/.vimrc
-    rm ~/.astylerc
-    exit
-fi
-
-VimDir=~/.vim/
-if [ ! -d $VimDir ]; then
-    echo "$VimDir not exists, will be create"
-    mkdir $VimDir
-else
-    echo "$VimDir exists."
-fi
+mkdir ~/.config/nvim/
+cp init.vim ~/.config/nvim
